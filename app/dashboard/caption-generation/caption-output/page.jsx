d1aa475da741c5cf1e101dc6935a296f21026c89
arnav0511 <saini05112003@gmail.com>
2024-11-20 08:29:25 +0530

    segmentation3

--- app/dashboard/caption-generation/caption-output/page.jsx
+++ app/dashboard/caption-generation/caption-output/page.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import PromptOutput from '../../content/_components/PromptOutput'
+  import React from 'react'
+  import PromptOutput from '../../content/_components/PromptOutput'
 
-function CaptionOutput() {
-  return (
-    <div>
-        <PromptOutput/>
-    </div>
-  )
-}
+  function CaptionOutput() {
+    return (
+      <div>
+          <PromptOutput/>
+      </div>
+    )
+  }
 
-export default CaptionOutput
+  export default CaptionOutput
